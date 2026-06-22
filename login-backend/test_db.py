1f9cc7eeef31b59479cd234f524bef7f6c5a1d5d
Jeevitha-98 <jeevithakarunanithi98@gmai.l.com>
2026-06-22 17:52:24 +0530

    changes

--- login-backend/test_db.py
+++
@@ -1,8 +0,0 @@
-from database import engine
-
-try:
-    connection = engine.connect()
-    print("Database Connected Successfully")
-except Exception as e:
-    print("Connection Failed")
-    print(e)
